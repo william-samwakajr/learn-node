1ce84348e03dfa227d24a5c7c667174c07f57554
william-samwakajr <williamsamwaka05@gmail.com>
2024-01-25 12:05:59 +0200

    created a server

--- core-modules.mjs
+++ core-modules.mjs
@@ -4,7 +4,7 @@ import * as http from 'node:http';
 http.createServer((req, res) => {
     fs.readFile('importantInfo.html', function(err, data) {
         res.writeHead(200, { 'content-type': 'text/html' })
-        res.write(data)
+        res.write(data);
         res.end();
     });
 }).listen(8080);
