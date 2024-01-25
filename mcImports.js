172f2531d1b2e3a095225d52f44dce2470602c6b
william-samwakajr <williamsamwaka05@gmail.com>
2024-01-25 14:38:57 +0200

    made a unit convertor app that uses modules to seprate code

--- mcImports.js
+++ mcImports.js
@@ -4,6 +4,12 @@ const metricConverion = require('./metricConverions');
 const CovertToMetric = (value, unit) => {
     switch (unit) {
         case 'in':
-            return metricConverion.inchesToCentimeters(value)
+            return metricConverion.inchesToCentimeters(value);
+        case 'gl':
+            return metricConverion.gallonsToLiters(value);
+        case 'lb':
+            return metricConverion.poundsToKilograms(value);
+        case 'mi':
+            return metricConverion.milesToKilometers(value)
     }
 }
